82444ddcefc1bec3eb660965df880f992394870a
Connor Warme <connor.warme@gmail.com>
2024-08-21 13:35:40 -0600

    Initial setup w/ linter, prettier, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return ( 
+    <>
+      <h1 className="bg-red-600">React App</h1>
+      <p>React App with TypeScript</p>
+    </>
+  );
+}
+
+export default App;
