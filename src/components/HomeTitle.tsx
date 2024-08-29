0ea49a1a0b45acc81fd6a66c0541bf05c72ddc41
Connor Warme <connor.warme@gmail.com>
2024-08-29 15:24:23 -0600

    Continue working on home page

--- src/components/HomeTitle.tsx
+++ src/components/HomeTitle.tsx
@@ -6,8 +6,8 @@ export const Title = () => {
   return ( 
     <div className="h-dvh lg:h-2/3 relative">
       <img src={contemplative} alt="Contemplative Woman, Head in her Hands, in Golden Light" className="h-full object-cover"/>
-      <div className="absolute top-4 left-4 text-center bg-midnight bg-opacity-75 p-4 px-6 rounded-md">
-        <h1 className="text-4xl font-bold text-sand flex gap-2 underline decoration-coral decoration-double" title="Mind Matters Memphis">Mind Matters Memphis</h1>
+      <div className="absolute top-1/4 left-4 text-center bg-midnight bg-opacity-85 p-8 px-6 rounded-md">
+        <h1 className="text-4xl font-bold text-sand flex gap-2 underline decoration-coral decoration-double pb-4" title="Mind Matters Memphis">Mind Matters Memphis</h1>
         <ul>
           <li>
             <Arrow className="inline-block text-coral" />
@@ -19,13 +19,6 @@ export const Title = () => {
           </li>
         </ul>
       </div>
-      {/* <div className="text-center bg-dust bg-opacity-25 z-10">
-        <h1 className="text-5xl font-bold text-white flex flex-col gap-4 items-start" title="Mind Matters Memphis">
-          <span>Mind</span>
-          <span className="pl-8">Matters</span>
-          <span className="pl-16">Memphis</span>
-        </h1>
-      </div> */}
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,55 @@
 import { Title } from "../components/HomeTitle";
+import calm from "../assets/imgs/Calm.jpg"
+import contemplative from "../assets/imgs/InDeepThought.jpg"
+import { ListItem } from "../components/HomeListItem";
 export const Home = () => {
       // background image of initial block
       // Mind Matters Memphis title
       // buttons for Learn More and Contact
       // block for Our Mission
       //
+      const listItems = [
+        {
+          title: "Clinical Expertise",
+          text: "Dr. Gould has been a neuropsychological clinician for 15 years.",
+          url: "/clinical-neuropsychology"
+        },
+        {
+          title: "Forensic Evaluations",
+          text: "Specialized forensic evaluations for legal and court-related matters.",
+          url: "/forensic-evaluations"
+        },
+        {
+          title: "Cognitive Aging",
+          text: "Support and strategies for healthy cognitive aging and memory care.",
+          url: "/cognitive-aging"
+        }
+      ]
   return ( 
     <div>
       <Title />
+      <div className="bg-dust lg:grid lg:grid-cols-4">
+        <div className="hidden lg:block col-span-2 p-12">
+          <img src={calm} alt="Young East Asian Woman in Cozy Work Environment" className="object-cover rounded-xl"/>
+        </div>
+        <div className="text-2xl p-4 pb-12 flex flex-col justify-center text-center lg:col-span-2 lg:text-4xl lg:text-start">
+          <h2 className="text-midnight py-4">Our Mission</h2>
+          <p className="text-inlet">Providing comprehensive neuropsychological care to enhance cognitive health and well being.</p>
+        </div>
+      </div>
+      <div className="bg-midnight p-4 lg:grid lg:grid-cols-4">
+        <div className="flex flex-col justify-center lg:col-span-2 lg:px-8">
+          <h2 className="text-sand py-4 self-center lg:text-4xl">Why Choose Us</h2>
+          <ul className="">
+            {
+              listItems.map((item, index) => <ListItem key={index} title={item.title} text={item.text}/>)
+            }
+          </ul>
+        </div>
+        <div className="hidden lg:block col-span-2 p-12">
+          <img src={contemplative} alt="Young Black Man in Contemplation" className="object-cover rounded-xl"/>
+        </div>
+      </div>
     </div>
   );
 }
