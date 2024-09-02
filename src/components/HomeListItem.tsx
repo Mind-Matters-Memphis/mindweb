23300b520c2205046bfd1f0e4a0ad6776a2bd311
Connor Warme <connor.warme@gmail.com>
2024-09-01 20:57:15 -0600

    Add link to home list items, needs animation

--- src/components/HomeListItem.tsx
+++ src/components/HomeListItem.tsx
@@ -1,19 +1,23 @@
 import { IconType } from "react-icons";
+import { Link } from "react-router-dom";
 
 type itemProps = {
   title: string;
   text: string;
   icon: IconType;
   imgText: string;
+  url: string;
 }
-export const ListItem = ({title, text, icon, imgText}: itemProps) => {
+export const ListItem = ({title, text, icon, imgText, url}: itemProps) => {
   return ( 
-    <li className="flex bg-inlet bg-opacity-60 items-center p-4 px-8 rounded-full m-4">
-      { icon({className:"inline-block h-12 w-12 text-coral", "aria-label": `${imgText}`}) }
-      <div className="p-2">
-        <h3 className="text-2xl font-bold">{title}</h3>
-        <p>{text}</p>
-      </div>
+    <li className="bg-inlet bg-opacity-60 p-4 px-8 rounded-full m-4">
+      <Link to={url} className="flex items-center" aria-label={title}>
+        { icon({className:"inline-block h-12 w-12 text-coral", "aria-label": `${imgText}`}) }
+        <div className="p-2">
+          <h3 className="text-2xl font-bold">{title}</h3>
+          <p>{text}</p>
+        </div>
+      </Link>
     </li>
   );
 }
