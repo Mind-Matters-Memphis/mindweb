6d274b726a9a7542c18e1b1add3608a9415ac40f
Connor Warme <connor.warme@gmail.com>
2024-09-06 16:26:16 -0600

    Continue on services section - cards, styling, lists

--- src/components/HomeServices.tsx
+++ src/components/HomeServices.tsx
@@ -9,8 +9,16 @@ export const HomeServices = () => {
       title: "Clinical Neuropsychology",
       text: "Comprehensive neuropsychological evaluations for individuals with cognitive concerns.",
       url: "/clinical-neuropsychology",
+      points: [
+        "Lorem ipsum list item",
+        "Lorem ipsum list item",
+        "Lorem ipsum list item",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+      ],
       linkText: "Learn More",
-      style: "justify-self-end",
+      style: "justify-self-end mr-24 mt-10 border-inlet from-inlet/75",
+      btnStyle: "border-inlet/75",
+      color: "inlet",
       icon: Exo,
       iconAlt: "Triangle, half solid, half streaked",
       rotation: 270,
@@ -19,8 +27,15 @@ export const HomeServices = () => {
       title: "Forensic Evaluations",
       text: "Expert forensic evaluations for legal matters involving cognitive health.",
       url: "/forensic-evaluations",
+      points: [
+        "Lorem ipsum list item",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        "Lorem ipsum list item",
+      ],
       linkText: "Learn More",
-      style: "justify-self-center",
+      style: "justify-self-center border-sage from-sage/75",
+      btnStyle: "border-sage/90",
+      color: "sage",
       icon: Cloud,
       iconAlt: "Triangle, half solid, half streaked",
       rotation: 65,
@@ -29,8 +44,16 @@ export const HomeServices = () => {
       title: "Memory Care",
       text: "Specialized care for individuals with memory disorders and their families.",
       url: "/memory-care",
+      points: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        "Lorem ipsum list item",
+        "Lorem ipsum list item",
+        "Lorem ipsum list item",
+      ],
       linkText: "Learn More",
-      style: "justify-self-start",
+      style: "justify-self-start ml-24 mb-10 border-coral from-coral/50",
+      btnStyle: "border-coral/70",
+      color: "coral",
       icon: Honeycomb,
       iconAlt: "Triangle, half solid, half streaked",
       rotation: 75,
@@ -42,7 +65,7 @@ export const HomeServices = () => {
     //     <Block key={index} service={service} invert={index % 2 === 0} />
     //   ))}
     // </div>
-    <div className="bg-dust relative grid min-h-dvh gap-8">
+    <div className="bg-dust relative grid min-h-dvh gap-10 ">
       <div className="h-[384px] w-[384px] rounded-br-full bg-inlet absolute top-0 left-0"></div>
       <div className="absolute bottom-0 right-0 border-b-0 border-dust border-t-75 border-r-75 border-r-coral z-10"></div>
       {services.map((service, index) => (
--- src/components/ServiceBlock.tsx
+++ src/components/ServiceBlock.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { IconType } from "react-icons";
+import { MdOutlineArrowRight as Arrow } from "react-icons/md";
 type Service = {
   title: string;
   text: string;
+  points?: string[];
   url: string;
   linkText: string;
   style: string;
+  btnStyle: string;
+  color: string;
   icon: IconType;
   iconAlt: string;
   rotation?: number;
@@ -14,28 +18,29 @@ type Props = {
   invert?: boolean;
   service: Service;
 }
-export const ServiceBlock = ({ invert, service }: Props) => {
-
-  const iconStyle = `h-full w-auto text-coral`
-  const col1 = invert ? " col-start-2" : " col-start-1";
-  const col2 = invert ? " col-start-1" : " col-start-3";
+export const ServiceBlock = ({ service }: Props) => {
 
   return ( 
-    <div className={"z-20 " + service.style}>
-      <div className={"bg-midnight grid justify-center items-center w-fit p-8 rounded-3xl"}>
+    <div className={"p-8 border-2 rounded-3xl shadow-2xl max-w-3xl bg-gradient-to-tl to-dust z-20 " + service.style}>
+      <div className={"bg-dust/75 text-midnight shadow-inner grid justify-center items-center w-fit p-8 rounded-3xl"}>
         <div>
           <h2 className="text-4xl font-accessdisplay">{service.title}</h2>
         </div>
         <div>
           <p className="text-xl">{service.text}</p>
         </div>
-        <ul>
-          <li>{service.title}</li>
-          <li>{service.title}</li>
-          <li>{service.title}</li>
+        <ul className="">
+          {
+            service.points?.map((point, index) => (
+              <li key={index} className={`text-lg grid grid-cols-12 items-start list-inside`}>
+                <Arrow className={"h-8 w-8 justify-self-end text-" + service.color}/>
+                <span className="text-wrap col-span-10">{point}</span>
+              </li>
+            ))
+          }
         </ul>
         <div className="flex justify-center">
-          <Link to={service.url} className="text-2xl border-2 p-4 m-4 border-coral rounded-xl">{service.linkText}</Link>
+          <Link to={service.url} className={"text-2xl p-4 px-8 m-4 rounded-lg text-midnight shadow-md border-4 " + service.btnStyle}>{service.linkText}</Link>
         </div>
       </div>
       {/* <div className={"bg-midnight grid justify-center items-center"}>
