1794d1f50590cf09c4b5531700d78f8db6507bb1
Connor Warme <connor.warme@gmail.com>
2024-09-11 19:45:55 -0600

    Add shadcn for ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      dust: '#F2F2F2',
-      sand: '#E9E3DC',
-      sage: '#9DB8A8',
-      coral: '#D5573B',
-      inlet: '#4D7EA8',
-      midnight: '#272932'
-    },
-    fontFamily: {
-      'display': ['Lobster', "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      'altdisplay': ['Reem Kufi',  "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      'body': ['Playfair', "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
-      'altbody': ['Merriweather', "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
-      'accessdisplay': ["Rockwell", "Times New Roman", "Times", "serif"],
-      'accessbody': ["Tahoma", "Times New Roman", "Times", "sans-serif"],
-    },
-    extend: {
-      borderWidth: {
-        '12': '12.5cqh',
-        '16': '16.65cqh',
-        '25': '25cqh',
-        '33': '33.3cqh',
-        '50': '50cqh',
-        '75': '75cqh',
-        '100': '100cqh',
-      },
-    },
+  	colors: {
+  		dust: '#F2F2F2',
+  		sand: '#E9E3DC',
+  		sage: '#9DB8A8',
+  		coral: '#D5573B',
+  		inlet: '#4D7EA8',
+  		midnight: '#272932'
+  	},
+  	fontFamily: {
+  		display: ['Lobster', "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+  		altdisplay: ['Reem Kufi',  "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+  		body: ['Playfair', "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+  		altbody: ['Merriweather', "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+  		accessdisplay: ["Rockwell", "Times New Roman", "Times", "serif"],
+  		accessbody: ["Tahoma", "Times New Roman", "Times", "sans-serif"]
+  	},
+  	extend: {
+  		borderWidth: {
+  			'12': '12.5cqh',
+  			'16': '16.65cqh',
+  			'25': '25cqh',
+  			'33': '33.3cqh',
+  			'50': '50cqh',
+  			'75': '75cqh',
+  			'100': '100cqh'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
