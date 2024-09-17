835ff5a449b59cbb8c9655a124de9cca40c1cad6
Connor Warme <connor.warme@gmail.com>
2024-09-17 14:48:10 -0600

    feat: add Bebas font to match new logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,8 @@ export default {
   		midnight: '#272932'
   	},
   	fontFamily: {
-  		display: ['Lobster', "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
   		altdisplay: ['Reem Kufi',  "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-  		body: ['Playfair', "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
-  		altbody: ['Merriweather', "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
-  		accessdisplay: ["Rockwell", "Times New Roman", "Times", "serif"],
+  		accessdisplay: ["Bebas", "Times New Roman", "Times", "serif"],
   		accessbody: ["Tahoma", "Times New Roman", "Times", "sans-serif"]
   	},
   	extend: {
