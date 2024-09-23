490f11e3fad4073d38a42a6eb807bd4e72c431a4
Connor Warme <connor.warme@gmail.com>
2024-09-23 11:12:40 -0600

    style: refer a patient comp, responsive

--- src/components/HomeReferral.tsx
+++ src/components/HomeReferral.tsx
@@ -1,12 +1,12 @@
 
 export const HomeReferral = () => {
   return ( 
-    <div className="bg-dust flex justify-center p-8">
-      <div className="text-midnight max-w-[720px] h-96 bg-dust p-8 grid justify-center relative rounded-bl-xl">
-        <h2 className="text-2xl text-center lg:text-4xl xl:text-6xl font-altdisplay font-bold uppercase">Refer a Patient</h2>
+    <div className="bg-dust flex justify-center p-4 sm:p-8">
+      <div className="text-midnight bg-dust p-4 grid gap-4 justify-center relative rounded-bl-xl text-xl max-w-[500px] md:gap-8 md:max-w-[800px]">
+        <h2 className="text-4xl sm:text-6xl md:text-7xl text-center font-accessdisplay font-bold uppercase">Refer a Patient</h2>
         <p>We welcome referrals from medical professionals for patients with epilepsy, traumatic brain injury, stroke, neurovascular accidents, non-epileptic seizures, and pre-surgical evaluations.</p>
         <p>Please complete the referral form and fax it - along with patient records - to (901)-766-7550.</p>
-        <button className="bg-dust/80 font-bold text-2xl p-4 px-8 border-2 border-midnight w-fit h-fit justify-self-center rounded-lg transition-all ease-in-out duration-200 relative shadow-md lg:active:left-[1px] lg:active:top-[2px] lg:active:shadow-none">Download Referral Form</button>
+        <button className="bg-dust/80 font-bold text-2xl p-4 m-4 sm:p-6 sm:px-12 border-2 border-midnight w-fit h-fit justify-self-center rounded-lg transition-all ease-in-out duration-200 relative shadow-md active:left-[1px] active:top-[2px] active:shadow-none">Download Referral Form</button>
       </div>
     </div>
   );
