f710f9bccba59922e02ddce56dde66635f68b97c
Connor Warme <connor.warme@gmail.com>
2024-10-27 18:01:30 -0600

    feat: new & improved nav in header, responsive, accessible

---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import { ChevronDown } from "lucide-react";
+
+interface NavMenu {
+  className?: string;
+}
+interface ListItem {
+  className?: string;
+  children?: React.ReactNode;
+  title: string;
+  href: string;
+}
+interface Link {
+  href: string;
+  children?: string;
+  className?: string;
+}
+
+const Link = ({ href, ...props }: Link) => {
+  const location = useLocation();
+  const isActive = location?.pathname === href;
+
+  return (
+    <NavigationMenu.Link asChild active={isActive}>
+      <RouterLink to={href} className={linkStyle} {...props} />
+    </NavigationMenu.Link>
+  );
+};
+// currently lacking animation for dropdown
+// [data-motion] attribute missing from NavigationMenu.Content..?
+
+const linkStyle = "block p-8 text-sand transition-all duration-300 ease-in-out hover:text-midnight hover:bg-sand hover:shadow-md lg:hover:text-sand lg:hover:bg-midnight lg:relative lg:p-4 lg:before:absolute lg:before:bottom-[15%] lg:before:left-[50%] lg:before:h-[2px] lg:before:w-0 lg:before:origin-center lg:before:rounded-sm lg:before:bg-sand lg:before:transition-all lg:before:duration-300 lg:before:ease-in-out lg:before:content-[''] lg:hover:before:left-[0%] lg:hover:before:w-[100%] lg:active:left-[1px] lg:active:top-[2px] lg:active:shadow-none xl:mx-4";
+
+const NavMenu: React.FC<NavMenu> = () => (
+  <NavigationMenu.Root className="hidden relative z-50 lg:flex w-screen bg-midnight text-sand text-2xl shadow-sm group font-accessdisplay">
+    <NavigationMenu.List className="flex w-screen items-center">
+      <NavigationMenu.Item className="py-4 lg:py-8">
+        <Link href="/" className="p-4 lg:p-8 sm:text-4xl">
+          Mind Matters Memphis
+        </Link>
+      </NavigationMenu.Item>
+
+      <div id="spacer" className="grow"></div>
+
+      <NavigationMenu.Item>
+        <Link href="/meet-dr-gould">
+          About
+        </Link>
+      </NavigationMenu.Item>
+
+      <NavigationMenu.Item className="relative">
+        <NavigationMenu.Trigger className={linkStyle + " group flex items-center gap-3 peer"}>
+          <span>Clinical</span>
+          <ChevronDown className="transition-transform duration-[250ms] ease-in group-data-[state=open]:-rotate-180" />
+        </NavigationMenu.Trigger>
+        <NavigationMenu.Content className="absolute bg-midnight rounded-b-md -left-[50%] peer-data-[open=true]:animate-enterFromRight data-[motion=from-start]:animate-enterFromLeft data-[motion=to-end]:animate-exitToRight data-[motion=to-start]:animate-exitToLeft">
+          <ul className="flex flex-col justify-center p-4">
+            <ListItem href="/clinical-neuropsychology" title="Clinical Neuropsychology" className="" />
+            <ListItem href="/forms" title="Forms" className="flex" />
+          </ul>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+      <NavigationMenu.Item>
+        <Link href="/forensic-evaluations">
+          Forensic Evaluations
+        </Link>
+      </NavigationMenu.Item>
+      <NavigationMenu.Item>
+        <Link href="/contact" >
+          Contact
+        </Link>
+      </NavigationMenu.Item>
+    </NavigationMenu.List>
+  </NavigationMenu.Root>
+);
+
+
+const ListItem = React.forwardRef<HTMLAnchorElement, ListItem>(
+  ({ className, children, title, href, ...props }, forwardedRef) => (
+    <li>
+      <NavigationMenu.Link asChild>
+        <RouterLink
+          to={href}
+          className={"flex justify-center " + className}
+          {...props}
+          ref={forwardedRef}
+        >
+          <div className={linkStyle + " self-center text-nowrap"}>
+            {title}
+          </div>
+          <p className="">{children}</p>
+        </RouterLink>
+      </NavigationMenu.Link>
+    </li>
+  ),
+);
+
+
+export default NavMenu;
---
+++ src/components/NavMenuMobile.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import { ChevronDown, CornerDownRight } from "lucide-react";
+
+interface NavMenuMobile {
+  className?: string;
+}
+interface ListItem {
+  className?: string;
+  children?: React.ReactNode;
+  title: string;
+  href: string;
+}
+interface Link {
+  href: string;
+  children?: string;
+  className?: string;
+}
+
+const Link = ({ href, className, ...props }: Link) => {
+  const location = useLocation();
+  const isActive = location?.pathname === href;
+
+  return (
+    <NavigationMenu.Link asChild active={isActive}>
+      <RouterLink to={href} className={className + " " + linkStyle} {...props} />
+    </NavigationMenu.Link>
+  );
+};
+
+const linkStyle = "block text-sand transition-all duration-300 ease-in-out hover:text-midnight hover:bg-sand hover:shadow-md";
+
+const NavMenuMobile: React.FC<NavMenuMobile> = () => (
+  <NavigationMenu.Root className="relative z-50 flex w-screen bg-midnight text-sand text-2xl shadow-sm group font-accessdisplay lg:hidden">
+    <NavigationMenu.List className="flex w-screen items-center relative">
+      <NavigationMenu.Item className="">
+        <Link href="/" className="p-4 px-2 sm:px-6 lg:p-8 sm:text-4xl">
+          Mind Matters Memphis
+        </Link>
+      </NavigationMenu.Item>
+
+      <div id="spacer" className="grow"></div>
+
+      <NavigationMenu.Item>
+        <NavigationMenu.Trigger className="flex gap-4 items-center p-4 px-2 sm:px-6 group">
+          Menu
+          <ChevronDown className="transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180" />
+        </NavigationMenu.Trigger>
+        <NavigationMenu.Content className="absolute bg-midnight top-16 md:top-[72px] left-0 w-screen min-h-dvh" data-orientation="vertical" >
+          <NavigationMenu.Sub data-orientation="vertical" defaultValue="about">
+            <NavigationMenu.List>
+              <NavigationMenu.Item value="about">
+                <Link href="/meet-dr-gould" className="p-8">
+                  About
+                </Link>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item>
+                <Link href="/clinical-neuropsychology" className="p-8">
+                  Clinical Neuropsychology
+                </Link>
+                <div className="pl-12 flex gap-4 items-center">
+                  <CornerDownRight />
+                  <Link href="/forms" className="grow p-6">
+                    Forms
+                  </Link>
+                </div>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item>
+                <Link href="/forensic-evaluations" className="p-8">
+                  Forensic Evaluations
+                </Link>
+              </NavigationMenu.Item>
+              <NavigationMenu.Item>
+                <Link href="/contact" className="p-8">
+                  Contact
+                </Link>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu.Sub>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+    </NavigationMenu.List>
+  </NavigationMenu.Root>
+);
+
+
+const ListItem = React.forwardRef<HTMLAnchorElement, ListItem>(
+  ({ className, children, title, href, ...props }, forwardedRef) => (
+    <li>
+      <NavigationMenu.Link asChild>
+        <RouterLink
+          to={href}
+          className={"" + className}
+          {...props}
+          ref={forwardedRef}
+        >
+          <div className={linkStyle + " "}>
+            {title}
+          </div>
+          <p className="">{children}</p>
+        </RouterLink>
+      </NavigationMenu.Link>
+    </li>
+  ),
+);
+
+
+export default NavMenuMobile;
